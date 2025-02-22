7cd537fe69ca2aa1a0558391c8f683d3ab4e82a2
Phoori Chantima <alfa.phoori@gmail.com>
2025-02-22 19:37:55 +0700

    Update HwStatus_para_publish.py

--- Jazzy Jalisco/5_Parameters/HwStatus_para_publish.py
+++ Jazzy Jalisco/5_Parameters/HwStatus_para_publish.py
@@ -12,7 +12,7 @@ class HwStatusNode_Para(Node):
         self.robot_name_ = self.get_parameter("rb_name").value
         self.robot_number_ = self.get_parameter("rb_no").value
         
-        self.hw_status_publish_= self.create_publisher(HardwareStatus,"hardware_status", 10)
+        self.hw_status_publish_= self.create_publisher(HardwareStatus,"hardware_status_para", 10)
         self.timer_ = self.create_timer(1.0, self.publish_hw_status)
         self.get_logger().info("Hw_Status_Publish_Node Start Now!")
 
